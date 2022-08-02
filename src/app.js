c74bf46a60acc67c4302b992a3bfdde5430f6812
jorgeduje <julian.russo@team.wirall.com>
2022-08-02 18:44:44 -0300

    finish

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import express from "express";
 import config from "./config";
 import productsRoutes from "./routes/products.routes";
-
+const cors = require("cors")
 const app = express();
 
+app.use(cors())
 app.set("port", config.port);
 // middlewares
 app.use(express.json());
